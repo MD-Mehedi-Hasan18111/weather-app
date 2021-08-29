00f4831cf0c2363711d9c6a1136b18d3c6691bb4
MH-RaZa-Developer <webdevelopermehedi1@gmail.com>
2021-08-29 12:42:11 +0600

    Upated

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const loadweatherData = async () => {
     const cityName = document.getElementById('input-city').value.toLowerCase();
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=5465f34d91078c55f4fdd6bb937efeb6`;
+    document.getElementById('spinner').classList.remove('d-none');
     const res = await fetch(url);
     const data = await res.json();
     displayWeather(data);
@@ -12,10 +13,11 @@ const displayWeather = data => {
     const { main, icon } = data.weather[0];
     const { temp } = data.main;
     const parentDiv = document.getElementById('info');
+    document.getElementById('spinner').classList.add('d-none');
     parentDiv.innerHTML = `
         <img src="http://openweathermap.org/img/wn/${icon}@2x.png" />
         <h2>${data.name}</h2>
-        <h3>${temp}&deg;C</h3>
+        <h3>${(temp / 10).toFixed(0)}&deg;C</h3>
         <h1>${main}</h1>
     `
 }
